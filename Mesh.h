3ee72279ff21bbebfd8ac1879cf4c09c94f75562
EmiLoomistar <36137649+EmiLoomistar@users.noreply.github.com>
2026-03-24 07:25:47 -0600

    Add files via upload

---
+++ Mesh.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <glew.h>
+#include <vector>
+
+
+class Mesh
+{
+public:
+	Mesh();
+
+	void CreateMesh(GLfloat *vertices, unsigned int *indices, unsigned int numOfVertices, unsigned int numOfIndices);
+	void CreateMeshGeometry(std::vector<GLfloat>& vertices, std::vector<unsigned int>& indices, unsigned int numOfVertices, unsigned int numberOfIndices);
+	void RenderMesh();
+	void RenderMeshGeometry();
+	void ClearMesh();
+
+	~Mesh();
+
+private:
+	GLuint VAO, VBO, IBO;
+	GLsizei indexCount;
+};
+
